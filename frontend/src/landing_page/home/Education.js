01f532dc97ac510dfadeafae5a250be800fdf948
Akshat Dwivedi <akshatdwi34@gmail.com>
2026-02-09 23:37:23 +0530

    Home Page

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -1,7 +1,30 @@
 import React from 'react';
 function Education() {
     return ( 
-    <h1>Education</h1> 
+    <div className='container mt-5 p-5'>
+        <div className='row'>
+            <div className='col-6'>
+                <img src='media/images/education.svg' style={{width:"80%"}}/>
+            </div>
+
+            <div className='col-6 p-5'>
+                
+                <h1 className='fs-3 mb-5'>Free and open market education</h1>
+                <p> Varsity, the largest online stock market education book in the world covering 
+                everything from the basics to advanced trading.
+                </p>
+                <a href=''  style={{textDecoration:"none"}}>Varsity <i class="fas fa-long-arrow-alt-right"></i></a>
+
+                <p className='mt-4'>TradingQ&A, the most active trading and investment community 
+                    in India for all your market related queries.
+                </p>
+                <a href=''  style={{textDecoration:"none"}}>TradingQ&A <i class="fas fa-long-arrow-alt-right"></i></a>
+
+               
+             </div>
+
+        </div>
+    </div>
 );
 }
 
--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
  function Hero() {
-    return (  );
+    return (  <div></div>);
  }
  
  export default Hero;
