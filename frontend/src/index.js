6a7cde32b311cc3c6776120603cd8a7aa9191702
Akshat Dwivedi <akshatdwi34@gmail.com>
2026-03-03 22:35:18 +0530

    final

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter , Routes , Route} from 'react-router-dom';
-import './index.css';
-import HomePage from './landing_page/home/HomePage';
-import Signup from './landing_page/signup/SignUp';
-import About from './landing_page/about/AboutPage';
-import Products from './landing_page/product/ProductPage';
-import Pricing from './landing_page/pricing/PricingPage';
-import Support from './landing_page/support/SupportPage';
-import Footer from './landing_page/Footer';
-import Navbar from './landing_page/Navbar';
-import NotFound from './landing_page/NotFound';
-import PricingCurrency from './landing_page/pricing/PricingCurrency';
-import PricingCommodity from './landing_page/pricing/PricingCommodity';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import HomePage from "./landing_page/home/HomePage";
+import Signup from "./landing_page/signup/SignUp";
+import About from "./landing_page/about/AboutPage";
+import Products from "./landing_page/product/ProductPage";
+import Pricing from "./landing_page/pricing/PricingPage";
+import Support from "./landing_page/support/SupportPage";
+import Footer from "./landing_page/Footer";
+import Navbar from "./landing_page/Navbar";
+import NotFound from "./landing_page/NotFound";
+import PricingCurrency from "./landing_page/pricing/PricingCurrency";
+import PricingCommodity from "./landing_page/pricing/PricingCommodity";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <Navbar/>
-  <Routes>
-    <Route path='/' element = {<HomePage/>} />
-    <Route path='/signup' element ={<Signup/>} />
-    <Route path='/about' element = {<About/>} />
-    <Route path='/products' element = {<Products/>} />
-    <Route path='/pricing' element = {<Pricing/>} />
-     <Route path='/support' element = {<Support/>} />
-     <Route path="/pricing/Currency" element = {<PricingCurrency/>} />
-      <Route path="/pricing/Commodity" element = {<PricingCommodity/>} />
-      <Route path='*' element = {<NotFound/>} />
-  </Routes>
-  <Footer/>
-  </BrowserRouter>
-  
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/support" element={<Support />} />
+      <Route path="/pricing/Currency" element={<PricingCurrency />} />
+      <Route path="/pricing/Commodity" element={<PricingCommodity />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>,
 );
-
